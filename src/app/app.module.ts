a7bbe40e3fccd68928390c5bb7d4178accfe81e0
wuwj-cn <wenjie.0617@gmail.com>
2017-12-15 11:10:30 +0800

    commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,12 +12,31 @@ import { CoreModule }       from './core/core.module';
 /* Routing Module */
 import { AppRoutingModule } from './app-routing.module';
 
+import { HttpClient } from '@angular/common/http';
+import { TranslateHttpLoader } from '@ngx-translate/http-loader';
+import { TranslateLoader, TranslateModule } from '@ngx-translate/core';
+
+// AoT requires an exported function for factories
+export function createTranslateLoader(http: HttpClient) {
+  // for development
+  // return new TranslateHttpLoader(http, '/mplus-web/dist/assets/i18n/', '.json');
+  return new TranslateHttpLoader(http, './assets/i18n/', '.json');
+}
+
+
 @NgModule({
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
     CoreModule.forRoot({userName: 'Miss Marple'}),
     AppRoutingModule,
+    TranslateModule.forRoot({
+      loader: {
+        provide: TranslateLoader,
+        useFactory: createTranslateLoader,
+        deps: [HttpClient]
+      }
+    })
   ],
   declarations: [ AppComponent],
   bootstrap:    [ AppComponent ]
--- src/app/main/main.module.ts
+++ src/app/main/main.module.ts
@@ -2,32 +2,14 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { MainRoutingModule } from './main-routing.module';
-import { TranslateModule } from '@ngx-translate/core';
-import { TranslateLoader } from '@ngx-translate/core';
-import { HttpClient } from '@angular/common/http';
-import { TranslateHttpLoader } from '@ngx-translate/http-loader';
 import { MainComponent } from './main.component';
 import { HeaderComponent } from './header/header.component';
 import { SidebarComponent } from './sidebar/sidebar.component';
 
-// AoT requires an exported function for factories
-export function createTranslateLoader(http: HttpClient) {
-  // for development
-  // return new TranslateHttpLoader(http, '/mplus-web/dist/assets/i18n/', '.json');
-  return new TranslateHttpLoader(http, './assets/i18n/', '.json');
-}
-
 @NgModule({
   imports: [
     CommonModule,
-    MainRoutingModule,
-    TranslateModule.forRoot({
-      loader: {
-        provide: TranslateLoader,
-        useFactory: createTranslateLoader,
-        deps: [HttpClient]
-      }
-    })
+    MainRoutingModule
   ],
   declarations: [MainComponent, HeaderComponent, SidebarComponent]
 })
