3c61aa1dc2b5a8d9112574a8a4943df24d265c62
wuwj-cn <wenjie.0617@gmail.com>
2017-12-28 14:04:58 +0800

    commit

--- src/app/login/login.module.ts
+++ src/app/login/login.module.ts
@@ -3,10 +3,12 @@ import { CommonModule } from '@angular/common';
 
 import { LoginRoutingModule } from './login-routing.module';
 import { LoginComponent } from './login.component';
+import { TranslateModule } from '@ngx-translate/core';
 
 @NgModule({
   imports: [
     CommonModule,
+    TranslateModule,
     LoginRoutingModule
   ],
   declarations: [LoginComponent]
