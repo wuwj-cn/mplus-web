27f6a13e113b70a1fc2d9bacc909758f485d0ef2
wuwj-cn <wenjie.0617@gmail.com>
2018-01-09 19:00:17 +0800

    commit

--- src/app/pages/users/users.component.ts
+++ src/app/pages/users/users.component.ts
@@ -1,9 +1,11 @@
 import { Component, OnInit } from '@angular/core';
+import { routerTransition } from '../../router.animations';
 
 @Component({
   selector: 'app-users',
   templateUrl: './users.component.html',
-  styleUrls: ['./users.component.css']
+  styleUrls: ['./users.component.css'],
+  animations: [routerTransition()]
 })
 export class UsersComponent implements OnInit {
 
--- src/app/pages/users/users.module.ts
+++ src/app/pages/users/users.module.ts
@@ -3,11 +3,13 @@ import { CommonModule } from '@angular/common';
 
 import { UsersRoutingModule } from './users-routing.module';
 import { UsersComponent } from './users.component';
+import { PageHeaderModule } from '../../shared/modules/page-header/page-header.module';
 
 @NgModule({
   imports: [
     CommonModule,
-    UsersRoutingModule
+    UsersRoutingModule,
+    PageHeaderModule
   ],
   declarations: [UsersComponent]
 })
