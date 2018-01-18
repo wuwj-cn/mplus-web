407d38b4bb0c702b0aa4ab64fcb56b57743cc3b7
wuwj-cn <wenjie.0617@gmail.com>
2018-01-18 19:02:21 +0800

    解决header布局问题

--- src/app/pages/users/users.component.ts
+++ src/app/pages/users/users.component.ts
@@ -8,29 +8,86 @@ import { routerTransition } from '../../router.animations';
   animations: [routerTransition()]
 })
 export class UsersComponent implements OnInit {
-  private page = 1;
-  private totalSize = 80;
-  private users: any[];
-  private pageSize = 10;
-
 
   constructor() {
-    this.users = [
-      {username: 'wuwj', email: 'wenjie.0617@gmail.com', fullname: 'wuwenjie'},
-      {username: 'wuwj', email: 'wenjie.0617@gmail.com', fullname: 'wuwenjie'},
-      {username: 'wuwj', email: 'wenjie.0617@gmail.com', fullname: 'wuwenjie'},
-      {username: 'wuwj', email: 'wenjie.0617@gmail.com', fullname: 'wuwenjie'},
-      {username: 'wuwj', email: 'wenjie.0617@gmail.com', fullname: 'wuwenjie'}
-
-    ]
   }
 
   ngOnInit() {
   }
 
-  onPageSizeChange(pageSize: number) {
-    this.pageSize = pageSize;
-    console.log("page size changed....");
+  filterNameArray = [
+    { name: 'Joe', value: false },
+    { name: 'Jim', value: false },
+  ];
+  filterAddressArray = [
+    { name: 'London', value: false },
+    { name: 'Sidney', value: false }
+  ];
+  sortMap = {
+    name   : null,
+    age    : null,
+    address: null
+  };
+  sortName = null;
+  sortValue = null;
+  data = [
+    {
+      name   : 'John Brown',
+      age    : 32,
+      address: 'New York No. 1 Lake Park',
+    }, {
+      name   : 'Jim Green',
+      age    : 42,
+      address: 'London No. 1 Lake Park',
+    }, {
+      name   : 'Joe Black',
+      age    : 32,
+      address: 'Sidney No. 1 Lake Park',
+    }, {
+      name   : 'Jim Red',
+      age    : 32,
+      address: 'London No. 2 Lake Park',
+    }
+  ];
+  copyData = [ ...this.data ];
+
+  sort(sortName, value) {
+    this.sortName = sortName;
+    this.sortValue = value;
+    Object.keys(this.sortMap).forEach(key => {
+      if (key !== sortName) {
+        this.sortMap[ key ] = null;
+      } else {
+        this.sortMap[ key ] = value;
+      }
+    });
+    this.search();
+  }
+
+  search() {
+    const searchAddress = this.filterAddressArray.filter(address => address.value);
+    const searchName = this.filterNameArray.filter(name => name.value);
+    const filterFunc = (item) => {
+      return (searchAddress.length ? searchAddress.some(address => item.address.indexOf(address.name) !== -1) : true) &&
+        (searchName.length ? searchName.some(name => item.name.indexOf(name.name) !== -1) : true)
+    };
+    this.data = [ ...this.copyData.filter(item => filterFunc(item)) ];
+    this.data = [ ...this.data.sort((a, b) => {
+      if (a[ this.sortName ] > b[ this.sortName ]) {
+        return (this.sortValue === 'ascend') ? 1 : -1;
+      } else if (a[ this.sortName ] < b[ this.sortName ]) {
+        return (this.sortValue === 'ascend') ? -1 : 1;
+      } else {
+        return 0;
+      }
+    }) ];
+  }
+
+  reset(array) {
+    array.forEach(item => {
+      item.value = false;
+    });
+    this.search();
   }
 
 }
