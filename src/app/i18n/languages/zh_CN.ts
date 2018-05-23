b952b3ab1903ed06bc1e2f8ee7feacbd94268c4d
wuwj-cn <wenjie.0617@gmail.com>
2018-05-23 18:29:00 +0800

    commit

--- src/app/i18n/languages/zh_CN.ts
+++ src/app/i18n/languages/zh_CN.ts
@@ -1,7 +1,11 @@
 export default {
     locale: 'zh-cn',
     base: {
-        action: '操作'
+        action: '操作',
+        add: '增加',
+        search: '查询',
+        edit: '编辑',
+        delete: '删除'
     },
     user: {
         userCode: '用户编号',
